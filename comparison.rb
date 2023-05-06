61452828a8ac30a87db623363939307d3d59deb8
Aakarshit-Dhuria <aakarshitdhuria@gmail.com>
2023-05-06 19:15:52 +0530

    Added comparison operators

---
+++ comparison.rb
@@ -0,0 +1,19 @@
+# ==
+# !=
+# <
+# >
+
+# puts 10 == 10
+# puts "10" == 10
+# puts "10".to_f == 10
+# puts 10.1 == 10
+
+# puts "hello" == "bye"
+# puts "hello" != "bye"
+ 
+puts 10 === 10.0
+
+## To check if they even have same type
+puts 10.eql?(10.0)
+puts 1.0.eql?(10.0)
+
