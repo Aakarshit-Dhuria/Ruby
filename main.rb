7560b35867896bab2f65756be2d76487c4ae3ec4
Aakarshit-Dhuria <aakarshitdhuria@gmail.com>
2023-05-06 14:23:08 +0530

    Introduction to ruby

--- main.rb
+++ main.rb
@@ -1,1 +1,22 @@
-puts "Hello, world"
+# puts "Hello, world" # creates a new line character.
+# p "Hello, world"    # also returns the string along with printing it.
+# print "Hello, world" # doesnt add a new line character at end.
+
+
+# greeting  = "Hello, world"
+# puts greeting 
+
+## Function without any argument
+# def say_hello
+#     puts "message"
+# end
+
+# say_hello
+
+
+## Function with an argument
+def say_hello(message)
+    puts message
+end
+
+say_hello "hello aakarshit"
