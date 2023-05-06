0365e53a752975a2c1c094eebad966c1deeeee4b
Aakarshit-Dhuria <aakarshitdhuria@gmail.com>
2023-05-06 19:32:14 +0530

    Added ifelse conditionals

---
+++ ifelse.rb
@@ -0,0 +1,24 @@
+condition = true
+another_condition = false
+
+# if condition && another_condition
+# if condition || another_condition
+# if !condition && another_condition
+# if (condition && !another_condition)
+#     puts "true"
+# else
+#     puts "false"
+# end
+# puts "la la la"
+
+
+name = "Aakarshit"
+if name == "Aakarshit"
+    puts "Hi, Aakarshit"
+elsif name == "Liza"
+    puts "Hi, Liza"
+elsif name == "New User"
+    puts "Hi, New User"
+else
+    puts "Hi, unknown user"
+end
